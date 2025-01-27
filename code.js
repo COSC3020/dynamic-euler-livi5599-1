bdf2f70d48364a89496bec23c023324143706a5e
livi5599 <obrown1@uwyo.edu>
2025-01-27 12:07:34 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,21 @@
 function factorial(n) {
-    if(n === 0) return 1;
-    else return n * factorial(n - 1);
+    let table = new Array(n+1);
+    table[0] = 1;
+    table[1] = 1;
+
+    for (i = 2; i <= n; i++) {
+        table[i] = i * table[i-1];
+    }
+    return table;
 }
 
 function e(n) {
-    if(n === 0) return 1;
-    else return 1.0 / factorial(n) + e(n - 1);
+    let eTable = new Array(n+1);
+    let facTable = factorial(n);
+    eTable[0] = 1;
+
+    for (i = 1; i <= n; i++) {
+        eTable[i] = 1.0 / facTable[i] + eTable[i-1];
+    }
+    return eTable[n];
 }
